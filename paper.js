a2a139988cfc5a9bb2e35b0300e24150e569155e
BananaAppleCoder <mohapatra9@gmail.com>
2021-06-25 20:32:36 -0500

    finish

--- paper.js
+++ paper.js
@@ -1,65 +1,32 @@
-class dustbin
-{
-	constructor(x,y)
-	{
-		this.x=x;
-		this.y=y;
-		this.dustbinWidth=200;
-		this.dustbinHeight=213;
-		this.wallThickness=20;
-		
-		this.image=loadImage("dustbingreen.png")
-		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-		
-		
+class paper{
+	constructor(x,y,r){
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+		}
 
-		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		this.image = loadImage("paper.png")
+		this.body=Bodies.circle(x, y, (r-30)/2	, options)
 		
-		World.add(world, this.bottomBody)
-		World.add(world, this.leftWallBody)
-		World.add(world, this.rightWallBody);
-
+		World.add(world, this.body)
+		this.x = x;
+		this.y = y;
+		this.r = r
 	}
-	display()
-	{
-			var posBottom=this.bottomBody.position;
-			var posLeft=this.leftWallBody.position;
-			var posRight=this.rightWallBody.position;
+	display(){
+			var pos = this.body.position;
 
+			push()
+
+			translate(pos.x, pos.y);
+			rectMode(CENTER)
+			fill(0)
+
+			imageMode(CENTER)
+			image(this.image, 0, 0, this.r, this.r)
 			
-
-			push()
-			translate(posLeft.x, posLeft.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			rotate(this.angle)
-			//rect(0,0,this.wallThickness, this.dustbinHeight);
 			pop()
-
-			push()
-			translate(posRight.x, posRight.y);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			rotate(-1*this.angle)
-			//rect(0,0,this.wallThickness, this.dustbinHeight);
-			pop()
-
-			push()
-			translate(posBottom.x, posBottom.y+10);
-			rectMode(CENTER)
-			//strokeWeight(4);
-			angleMode(RADIANS)
-			fill(255)
-			imageMode(CENTER);
-			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
-			//rect(0,0,this.dustbinWidth, this.wallThickness);
-			pop()
-			
 	}
-
 }
--- sketch.js
+++ sketch.js
@@ -4,23 +4,23 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj,groundObject	
+var dustbinObj,groundObject, ball
 var world;
 
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);//!1600x700
+	createCanvas(1600, 700);
 	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
 	
-	groundObject=new ground(width/2,height*0.9571428571428571,width,20);
-	dustbinObj=new dustbin(width*0.75,height*0.9285714285714286);
+	groundObject=new ground(800, 670, 1600, 20);
+	dustbinObj=new dustbin(1200, 650);
+	ball = new paper(300, 350, 100)
 
 	Engine.run(engine);
-  
 }
 
 function draw() {
@@ -30,6 +30,15 @@ function draw() {
 
   groundObject.display();
   dustbinObj.display();
+  ball.display()
+}
 
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball.body, ball.body.position, {x:130, y:-145})
+	}
+	if (keyCode === DOWN_ARROW){
+		Matter.Body.applyForce(ball.body, ball.body.position, {x:-130, y:145})
+	}
 }
 
